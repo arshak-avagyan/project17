51ee205ac5fc4ac3dc77550c14b3c267393d6e83
aavagyan <aavagyan@omilia.com>
2023-01-04 14:08:05 -0800

    Bug fix

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -10,8 +10,8 @@ export const updateSettings = async (data, type) => {
       method: 'PATCH',
       url:
         type === 'password'
-          ? 'http://localhost:3000/api/v1/users/updatePassword'
-          : 'http://localhost:3000/api/v1/users/updateMe',
+          ? '/api/v1/users/updatePassword'
+          : '/api/v1/users/updateMe',
       data,
     });
     if (res.data.status === 'success') {
