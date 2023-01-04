0a01c88b9efb7b3e3f57823cb6c03e97d1a180cd
aavagyan <aavagyan@omilia.com>
2023-01-04 14:32:00 -0800

    updateSettings bug fix

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -16,6 +16,7 @@ export const updateSettings = async (data, type) => {
     });
     if (res.data.status === 'success') {
       showAlert('success', `${type.toUpperCase()} updated successfully`);
+      location.reload();
     }
   } catch (err) {
     showAlert('error', err.response.data.message);
